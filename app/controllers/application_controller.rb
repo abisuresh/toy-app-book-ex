0500057a71074dc9994ae954dab85aeb9e7b2af7
Abbi <suresha@oregonstate.edu>
2019-05-22 11:26:21 -0600

    add hello message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def hello
+    render html:"hello, word. this time we're creating a toy app."
+  end
 end
